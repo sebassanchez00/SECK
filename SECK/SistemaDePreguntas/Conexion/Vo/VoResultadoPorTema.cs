9adbe45ab5ee697618e5e798ef25ea70c5dec0c7
SebastianSanchezAristizabal <sebassanchez00@gmail.com>
2022-04-21 10:02:22 -0700

    Se añade funcionalidad que permite guardar los puntajes de las pruebas por cada tema evaluado

---
+++ SECK/SistemaDePreguntas/Conexion/Vo/VoResultadoPorTema.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapaDatos.Vo
+{
+    public class VoResultadoPorTema
+    {
+        public string ID_Evaluacion { get; set; }
+        public short ID_Tema { get; set; }
+        public string Enunciado_Tema { get; set; }
+        public float Puntaje { get; set; }
+
+        public VoResultadoPorTema()
+        { }
+
+        public VoResultadoPorTema(string iD_Evaluacion, short iD_Tema, string enunciado_Tema, float puntaje)
+        {
+            this.ID_Evaluacion = iD_Evaluacion;
+            this.ID_Tema = iD_Tema;
+            this.Enunciado_Tema = enunciado_Tema;
+            this.Puntaje = puntaje;
+        }
+    }
+}
